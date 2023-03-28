4d683f39b670d5df0f4b5ab00c06627ebcb073ee
Bill Merickel <billmerickel@Bills-MacBook-Pro-2.local>
2023-03-27 21:12:37 -0400

    Add multiple packages and package handling logic

--- Assets/Delivery.cs
+++ Assets/Delivery.cs
@@ -4,20 +4,26 @@ using UnityEngine;
 
 public class Delivery : MonoBehaviour
 {
+    bool hasPackage;
+    [SerializeField] float packageDestructionDelay = 0.5f;
+    
     void OnCollisionEnter2D()
     {
-        Debug.Log("COLLISION");
+        Debug.Log("Watch it!");
     }
 
     void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.tag == "Package")
+        if (other.tag == "Package" && !hasPackage)
         {
             Debug.Log("Package picked up");
+            hasPackage = true;
+            Destroy(other.gameObject, packageDestructionDelay);
         }
-        else if (other.tag == "Customer")
+        else if (other.tag == "Customer" && hasPackage)
         {
             Debug.Log("Package delivered!");
+            hasPackage = false;
         }
     }
 }
